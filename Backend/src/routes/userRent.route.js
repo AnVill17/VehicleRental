c28c7bc65186bd601d1aa07e1a9030e9a8476d40
Anupam Vinay <anupamvinay05@gmail.com>
2025-12-18 03:25:12 +0530

    added the security thing by checking vendor each time

--- Backend/src/routes/userRent.route.js
+++ Backend/src/routes/userRent.route.js
@@ -3,6 +3,7 @@ import { Router } from "express";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
 import { approveRent, getAvailableVehicles, getLenderRentRequests, rating, rejectRent, rentVehicle } from "../controllers/rental.controller.js";
+import { checkIsVendor } from "../middlewares/admin.middleware.js";
 
 
 const router= Router()
@@ -14,14 +15,14 @@ router.post("/rent", verifyJwt, rentVehicle);
 
 router.post("/rating", verifyJwt, rating);
 
-
-router.get("/requests", verifyJwt, getLenderRentRequests);
-
-
-router.patch("/:rentId/approve", verifyJwt, approveRent);
+ 
+router.get("/requests", verifyJwt, checkIsVendor, getLenderRentRequests);
 
 
-router.patch("/:rentId/reject", verifyJwt, rejectRent);
+router.patch("/:rentId/approve", verifyJwt,checkIsVendor, approveRent);
+
+
+router.patch("/:rentId/reject", verifyJwt,checkIsVendor, rejectRent);
 
 
 export default router
